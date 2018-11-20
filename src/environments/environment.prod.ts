2f2a450a4f99b1b85ce918bb087d9d0818cd6803
Kevin Atsma <kevinatsma@gmail.com>
2018-11-20 16:23:37 -0500

    Set up Firebase hosting and Database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA5brYVkysdLw3Kzi6T_34BIw2ULPb5zwk',
+    authDomain: 'lifelifting-app.firebaseapp.com',
+    databaseURL: 'https://lifelifting-app.firebaseio.com',
+    projectId: 'lifelifting-app',
+    storageBucket: 'lifelifting-app.appspot.com',
+    messagingSenderId: '298521225684'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA5brYVkysdLw3Kzi6T_34BIw2ULPb5zwk',
+    authDomain: 'lifelifting-app.firebaseapp.com',
+    databaseURL: 'https://lifelifting-app.firebaseio.com',
+    projectId: 'lifelifting-app',
+    storageBucket: 'lifelifting-app.appspot.com',
+    messagingSenderId: '298521225684'
+  }
 };
 
 /*
