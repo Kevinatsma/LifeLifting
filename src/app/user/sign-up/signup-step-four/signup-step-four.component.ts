005515200b425c0f915fbb11367fff890337bed4
Kevin Atsma <kevinatsma@gmail.com>
2018-12-14 02:00:08 -0500

    First layout setup for step 4 - signup (CSS).

--- src/app/user/sign-up/signup-step-four/signup-step-four.component.ts
+++ src/app/user/sign-up/signup-step-four/signup-step-four.component.ts
@@ -3,22 +3,47 @@ import { AuthService } from 'src/app/core/auth/auth.service';
 import { SpecialistService } from 'src/app/specialists/specialist.service';
 import { Observable } from 'rxjs';
 import { Specialist } from 'src/app/specialists/specialist.model';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-signup-step-four',
   templateUrl: './signup-step-four.component.html',
-  styleUrls: ['./signup-step-four.component.scss', './../signup-step-one/signup-steps.scss']
+  styleUrls: [
+    './signup-step-four.component.scss',
+    './../signup-step-two/signup-step-two.component.scss',
+    './../signup-step-one/signup-steps.scss']
 })
 export class SignupStepFourComponent implements OnInit {
   specialists: Observable<Specialist[]>;
+  chosenSpecialist: string;
+  backButton = false;
 
 
   constructor( public auth: AuthService,
-               public specialistService: SpecialistService
+               public specialistService: SpecialistService,
+               public router: Router,
+               private route: ActivatedRoute,
                ) { }
 
   ngOnInit() {
     this.specialists = this.specialistService.getSpecialists();
+    this.chosenSpecialist = null;
+  }
+
+  updateUser(user) {
+    const data = {
+      packageChoice: this.chosenSpecialist
+    };
+    this.auth.setUserData(data, user)
+    .then(() => {
+      this.router.navigate(['../step-three'], { relativeTo: this.route });
+    });
+  }
+
+  nextSpecialist() {
+    if (this.backButton === false) {
+      this.backButton = true;
+    }
   }
 
 }
