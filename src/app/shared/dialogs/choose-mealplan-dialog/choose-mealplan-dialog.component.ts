b7f943e52d4f3bc6f3710e44440229897e16f786
Kevin Atsma <kevinatsma@gmail.com>
2019-03-15 02:40:53 -0500

    Fixes mealplan choose dropdown in shopping list navigate popup

--- src/app/shared/dialogs/choose-mealplan-dialog/choose-mealplan-dialog.component.ts
+++ src/app/shared/dialogs/choose-mealplan-dialog/choose-mealplan-dialog.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { FormGroup, FormBuilder } from '@angular/forms';
+import { Mealplan } from './../../../mealplans/mealplan.model';
+import { MealplanService } from './../../../mealplans/mealplan.service';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-choose-mealplan-dialog',
@@ -11,8 +16,12 @@ import { FormGroup, FormBuilder } from '@angular/forms';
 export class ChooseMealplanDialogComponent implements OnInit {
   chooseMealplanForm: FormGroup;
   selectedMealplan;
+  mealplansCol: AngularFirestoreCollection<Mealplan>;
+  mealplans: Observable<Mealplan[]>;
 
   constructor( private fb: FormBuilder,
+               private auth: AuthService,
+               private afs: AngularFirestore,
                public matDialog: MatDialog,
                @Inject(MAT_DIALOG_DATA) public data: any) { }
 
@@ -20,6 +29,12 @@ export class ChooseMealplanDialogComponent implements OnInit {
     this.chooseMealplanForm = this.fb.group({
       mealplan: (''),
     });
+    const uid =  this.auth.currentUserId;
+    this.mealplansCol = this.afs.collection('mealplans', ref => ref.where('clientID', '==', `${uid}`));
+    this.mealplans = this.mealplansCol.valueChanges();
+    console.log(this.mealplans);
   }
+
+
 
 }
