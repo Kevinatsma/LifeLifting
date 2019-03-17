7b13789664f4ffbdc8af5934072676ccb7e139c0
Kevin Atsma <kevinatsma@gmail.com>
2019-03-16 19:36:45 -0500

    Updates dropdown list in choose mealplan dialog

--- src/app/shared/dialogs/choose-mealplan-dialog/choose-mealplan-dialog.component.ts
+++ src/app/shared/dialogs/choose-mealplan-dialog/choose-mealplan-dialog.component.ts
@@ -7,6 +7,8 @@ import { MealplanService } from './../../../mealplans/mealplan.service';
 import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { AuthService } from './../../../core/auth/auth.service';
 import { Observable } from 'rxjs';
+import { UserService } from './../../../user/user.service';
+import { User } from './../../../user/user.model';
 
 @Component({
   selector: 'app-choose-mealplan-dialog',
@@ -14,6 +16,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./choose-mealplan-dialog.component.scss']
 })
 export class ChooseMealplanDialogComponent implements OnInit {
+  user: User;
   chooseMealplanForm: FormGroup;
   selectedMealplan;
   mealplansCol: AngularFirestoreCollection<Mealplan>;
@@ -22,18 +25,29 @@ export class ChooseMealplanDialogComponent implements OnInit {
   constructor( private fb: FormBuilder,
                private auth: AuthService,
                private afs: AngularFirestore,
+               private userService: UserService,
                public matDialog: MatDialog,
-               @Inject(MAT_DIALOG_DATA) public data: any) { }
+               @Inject(MAT_DIALOG_DATA) public data: any) {
+               }
 
   ngOnInit() {
     this.chooseMealplanForm = this.fb.group({
       mealplan: (''),
     });
-    const uid =  this.auth.currentUserId;
-    this.mealplansCol = this.afs.collection('mealplans', ref => ref.where('clientID', '==', `${uid}`));
-    this.mealplans = this.mealplansCol.valueChanges();
+    this.getUser();
   }
 
 
+  getUser() {
+    const id = this.auth.currentUserId;
+    this.userService.getUserDataByID(id).subscribe(user => {
+      this.user = user;
+      this.getMealplans(user.uid);
+    });
+  }
 
+  getMealplans(uid) {
+    this.mealplansCol = this.afs.collection('mealplans', ref => ref.where('clientID', '==', `${uid}`));
+    this.mealplans = this.mealplansCol.valueChanges();
+  }
 }
