23759ae91c493ce4ca6c67e2ebaa6a02acda02fc
Kevin Atsma <kevinatsma@gmail.com>
2020-09-20 01:34:28 -0500

    Adds async chat indicator that lights up if new chats are available

--- src/app/dashboard/dashboard-top-nav/dashboard-top-nav.component.ts
+++ src/app/dashboard/dashboard-top-nav/dashboard-top-nav.component.ts
@@ -1,9 +1,13 @@
-import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { User } from './../../user/user.model';
 import { AuthService } from './../../core/auth/auth.service';
 import { Router } from '@angular/router';
 import { ClientService } from './../../clients/client.service';
 import { DashboardService } from '../dashboard.service';
+import { ChatThreadService } from 'src/app/chat/chat-thread.service';
+import * as _ from 'lodash';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-top-nav',
@@ -11,16 +15,44 @@ import { DashboardService } from '../dashboard.service';
   styleUrls: ['./dashboard-top-nav.component.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class DashboardTopNavComponent implements OnInit {
+export class DashboardTopNavComponent implements OnInit, OnDestroy {
   @Input() user: User;
+  destroy$: Subject<boolean> = new Subject<boolean>();
   replacementURL = './../../../../assets/img/icons/user-no-photo.svg';
+  hasUnreads = false;
 
   constructor( private auth: AuthService,
                public router: Router,
                private clientService: ClientService,
-               private dashboardService: DashboardService) { }
+               private dashboardService: DashboardService,
+               private threadService: ChatThreadService) { }
 
   ngOnInit() {
+    this.checkUnreads();
+  }
+
+  checkUnreads() {
+    this.threadService.getThreads().pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(this.setUnreadFlag);
+  }
+
+  setUnreadFlag = (threads) => {
+    let hasUnreads = false;
+    _.forEach(threads, thread => {
+      console.log('thread', thread);
+      if (!hasUnreads) {
+        if (thread.lastSenderId === `${this.auth.currentUserId}`) {
+          hasUnreads = false;
+        } else  if (thread.unreadMessages === 0 ) {
+          hasUnreads = false;
+        } else {
+          hasUnreads = true;
+        }
+      }
+    });
+    this.hasUnreads = hasUnreads;
+    console.log('has unreads', this.hasUnreads);
   }
 
   editUser(user) {
@@ -35,5 +67,10 @@ export class DashboardTopNavComponent implements OnInit {
 
   signOut() {
     this.auth.signOut();
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.complete();
   }
 }
